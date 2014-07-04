b557d9887d2cb0e6f972d893b85fab87307be8b6
Ryan Wilson <syndicated.life@gmail.com>
2014-07-03 18:28:16 -0700

    proper fix on base directory, updated package

--- FFXIVAPP.Plugin.Parse/Constants.cs
+++ FFXIVAPP.Plugin.Parse/Constants.cs
@@ -83,8 +83,9 @@ namespace FFXIVAPP.Plugin.Parse
         {
             get
             {
-                return Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly()
-                                                             .CodeBase).LocalPath);
+                var appDirectory = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly()
+                                                                         .CodeBase).LocalPath);
+                return Path.Combine(appDirectory, "Plugins", Plugin.PName);
             }
         }
 
